c2d78e88117c5496a5bb824d1ccf28b45aec4821
Alina0614 <157624954+Alina0614@users.noreply.github.com>
2024-12-12 11:32:42 +0300

    зачет
    зачет

---
+++ WpfApp29/Window1.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp29
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var Name = name.Text;
+            var NameVrach = vrach.Text;
+
+            if(NameVrach != "" &&  Name != "")
+            {
+                MessageBox.Show("Вы записались на прием!", "Уведомление", MessageBoxButton.OK, MessageBoxImage.Information);
+                MainWindow mainWindow = new MainWindow();
+                mainWindow.Show();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Убедитесь, что все поля заполнены!", "Уведомление", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Close();
+        }
+    }
+}
---
+++ WpfApp29/Window3.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp29
+{
+    /// <summary>
+    /// Логика взаимодействия для Window3.xaml
+    /// </summary>
+    public partial class Window3 : Window
+    {
+        public Window3()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var Name = name.Text;
+            var NameVrach = vrach.Text;
+            var Password = password.Password.ToString();
+            if (NameVrach != "" && Name != "" && Password != "")
+            {
+                Window5 mainWindow5 = new Window5();
+                mainWindow5.Show();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Убедитесь, что все поля заполнены!", "Уведомление", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+        }
+    }
+}
